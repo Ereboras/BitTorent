2b5b7d27ba4526f09e4cf809af77f935468ff192
AZZOUZ Mohamed <azzouzminfo@yahoo.fr>
2024-09-30 18:46:07 +0200

    Added representation of torrent data file with Python Torrent object

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from collections import OrderedDict
 from pieces.bencode import Encoder, Decoder
+from pieces.torrent import Torrent
+
+with open('torrent-files/example.torrent', 'rb') as f:
+	meta_info = f.read()
+	torrent = Decoder(meta_info).decode()
+
+print(Torrent(torrent))
---
+++ pieces/torrent.py
@@ -0,0 +1,84 @@
+from collections import OrderedDict
+import datetime
+
+class Torrent:
+
+	def __init__(self, data: OrderedDict):
+		if not isinstance(data, OrderedDict):
+			raise TypeError(f'Torrent data must be Ordered dict, data is {type(data)} instead')
+
+		self._importProperties(data)
+		self._importFiles(data)
+
+
+	def _importProperties(self, data : OrderedDict):
+		self.properties = dict()
+
+		if b'encoding' in data:
+			self.properties['encoding'] = data[b'encoding'].decode('ascii')
+		else:
+			self.properties['encoding'] = 'ascii'
+
+		if b'announce' in data:
+			self.properties['announce'] = data[b'announce'].decode(self.properties['encoding'])
+		if b'created by' in data:
+			self.properties['created-by'] = data[b'created by'].decode(self.properties['encoding'])
+		if b'creation date' in data:
+			self.properties['creation-date'] = datetime.datetime.fromtimestamp(data[b'creation date'])
+		if b'info' in data and b'name' in data[b'info']:
+			self.properties['name'] = data[b'info'][b'name'].decode(self.properties['encoding'])
+		if b'info' in data and b'length' in data[b'info']:
+			self.properties['length'] = data[b'info'][b'length'].decode(self.properties['encoding'])
+		if b'info' in data and b'piece length' in data[b'info']:
+			self.properties['piece-length'] = self._convertBytes(data[b'info'][b'piece length'])
+
+	def _importFiles(self, data : OrderedDict):
+		self.files = list()
+
+		if b'info' in data and b'files' in data[b'info']:
+			for file in data[b'info'][b'files']:
+				tmpDict = dict()
+				if b'path' in file:
+					tmpDict['path'] = file[b'path'][0].decode(self.properties['encoding'])
+					tmpDict['length'] = file[b'length']
+					self.files.append(tmpDict)
+
+	def _formatProperties(self, key, displayKey):
+		tmpStr = ''
+		if key in self.properties:
+			tmpStr += f'{displayKey: <16}: {self.properties[key]}\n'
+		return tmpStr
+	
+	def _formatFiles(self):
+		tmpStr = ''
+
+		for file in self.files:
+			tmpStr += f'{'Path': <16}: {file['path']}\n'
+			tmpStr += f'{'Length': <16}: {self._convertBytes(file['length'])}\n'
+			tmpStr += '|============================|\n'
+		return tmpStr
+
+	def _convertBytes(self, size):
+		for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+			if size < 1024.0:
+				return "%3.1f %s" % (size, x)
+			size /= 1024.0
+
+		return size
+
+	def __str__(self):
+		tmpStr = '|============================|\n'
+		tmpStr += '|=========| TORRENT |========|\n'
+		tmpStr += '|============================|\n'
+
+		tmpStr += self._formatProperties('announce', 'Announce')
+		tmpStr += self._formatProperties('created-by', 'Created by')
+		tmpStr += self._formatProperties('encoding', 'Encoding')
+		tmpStr += self._formatProperties('creation-date', 'Creation date')
+		tmpStr += self._formatProperties('name', 'Name')
+		tmpStr += self._formatProperties('length', 'Length')
+		tmpStr += self._formatProperties('piece-length', 'Piece length')
+
+		tmpStr += '|=========== FILES ==========|\n'
+		tmpStr += self._formatFiles()
+		return tmpStr
