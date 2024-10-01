05a96621d67a6ddfa40c5cb1d603cf2657cc8cc2
AZZOUZ Mohamed <azzouzminfo@yahoo.fr>
2024-10-01 16:37:09 +0200

    Main script is now in function instead of global scope

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from pieces.torrent import Torrent
 import argparse
 
-parser = argparse.ArgumentParser(
-	description='This BitTorrent client can be used to download file through BitTorrent protocol.',
-)
+def main():
+	parser = argparse.ArgumentParser(
+		description='This BitTorrent client can be used to download file through BitTorrent protocol.',
+	)
 
-parser.add_argument('filepath',help='The path to the torrent file')
-args = parser.parse_args()
+	parser.add_argument('filepath',help='The path to the torrent file')
+	args = parser.parse_args()
 
-print(Torrent(args.filepath))
+	print(Torrent(args.filepath))
+
+if __name__ == '__main__':
+	main()
